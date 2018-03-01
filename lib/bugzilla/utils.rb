87122c68918697e3943dc2a80d9d9d5314efe33f
Victor Pereira <vpereirabr@gmail.com>
2018-03-01 08:42:01 +0100

    moved code away from bzconsole to Bugzilla::Utils. added test for it

--- lib/bugzilla/utils.rb
+++ lib/bugzilla/utils.rb
@@ -23,5 +23,31 @@ module Bugzilla
                                 http_basic_auth_pass: uri.password, debug: opts[:debug])
       [xmlrpc, host]
     end
+    def read_config(opts)
+      fname = opts[:config].nil? ? @defaultyamlfile : opts[:config]
+      begin
+        # TODO: fix config file
+        # Psych doesnt allow Symbol as class
+        # conf = YAML.safe_load(File.open(fname).read)
+        conf = YAML.load(File.open(fname).read)
+      rescue Errno::ENOENT
+        conf = {}
+      end
+      conf.each do |_k, v|
+        load(v[:Plugin]) if v.is_a?(Hash) && v.include?(:Plugin)
+      end
+      conf
+    end # def read_config
+
+    def save_config(opts, conf)
+      fname = opts[:config].nil? ? @defaultyamlfile : opts[:config]
+      if File.exist?(fname)
+        st = File.lstat(fname)
+        if st.mode & 0o600 != 0o600
+          raise format('The permissions of %s has to be 0600', fname)
+        end
+      end
+      File.open(fname, 'w') { |f| f.chmod(0o600); f.write(conf.to_yaml) }
+    end # def save_config
   end
 end
