6f09255faaa9b25917df01aa254bbdbb3815d4c3
Victor Pereira <vpereirabr@gmail.com>
2018-03-02 12:20:38 +0100

    renamed directory of plugins to plugins

--- spec/bugzilla_utils_spec.rb
+++ spec/bugzilla_utils_spec.rb
@@ -21,7 +21,7 @@ describe Bugzilla::Utils do
     before do
       @f = Foo.new
       @conf = { bsc: { Name: 'SUSE Bugzilla', URL: 'https://bugzilla.suse.com',
-                       Plugin: 'ruby-bugzilla/nvbugzilla.rb' } }
+                       Plugin: 'plugins/nvbugzilla.rb' } }
       @opts = { config: 'foo.yaml' }
     end
     after do
@@ -37,7 +37,7 @@ describe Bugzilla::Utils do
     before do
       @f = Foo.new
       @conf = { bsc: { Name: 'SUSE Bugzilla', URL: 'https://bugzilla.suse.com',
-                       Plugin: 'ruby-bugzilla/nvbugzilla.rb' } }
+                       Plugin: 'plugins/nvbugzilla.rb' } }
       @opts = { config: 'foo.yaml' }
       @f.save_config(@opts, @conf)
     end
